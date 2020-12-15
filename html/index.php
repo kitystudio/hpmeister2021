25dc98710198f2fc3ec189c6b93b4b6185c59f38
kity <yuhei.kitano@gmail.com>
2020-12-15 16:27:52 +0900

    修正しました。

---
+++ html/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>hpmeister.com</title>
+</head>
+<body>
+  <div class="thecontent">
+    <header>
+      <div class="logo"><a href=""><img src="/_assets/logo.svg" alt="hpmeister logo" width="160"></a></div>
+      <nav></nav>
+    </header>
+    <section id="news"></section>
+    <section id="blog"></section>
+    <section id="servicea"></section>
+    <section id="prof"></section>
+    <footer></footer>
+  </div>
+</body>
+</html>
